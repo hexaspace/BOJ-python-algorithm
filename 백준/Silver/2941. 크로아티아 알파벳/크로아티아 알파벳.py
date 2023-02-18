e836cde687d5bcef54274b68ac2bb54b425ee7dc
hexaspace <hexa6do@naver.com>
2023-02-18 09:35:29 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,23 +1,7 @@
-import re
-
+# replace를 사용하는 방법
 sent = input().rstrip()
+cro = ['c=','c-','dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+for c in cro:
+    sent = sent.replace(c, '#')
 
-kro1 = re.compile('c=')
-kro2 = re.compile('c-')
-kro3 = re.compile('dz=')
-kro4 = re.compile('d-')
-kro5 = re.compile('lj')
-kro6 = re.compile('nj')
-kro7 = re.compile('s=')
-kro8 = re.compile('z=')
-
-count = len(sent)
-count -= len(kro1.findall(sent))
-count -= len(kro2.findall(sent))
-count -= len(kro3.findall(sent))  # dz=에서 하나 찾으면, z=에서 한번 더 찾아짐
-count -= len(kro4.findall(sent))
-count -= len(kro5.findall(sent))
-count -= len(kro6.findall(sent))
-count -= len(kro7.findall(sent))
-count -= len(kro8.findall(sent))
-print(count)
+print(len(sent))
