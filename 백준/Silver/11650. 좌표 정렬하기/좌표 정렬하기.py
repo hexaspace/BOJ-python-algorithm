1b6b9c665459e132d1da960db94cf0cd86c1b74d
hexaspace <hexa6do@naver.com>
2023-02-19 23:59:18 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4204 ms, Memory: 54084 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,8 @@
+t = int(input())
+arr = []
+for _ in range(t):
+    x, y = map(int, input().split())
+    arr.append([x,y])
+arr.sort(key=lambda x: (x[0],x[1]))
+for i in range(t):   
+    print(arr[i][0], arr[i][1])
