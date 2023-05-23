462ae98c65dd4bbabaacd818a9756957dcbadec9
hexaspace <hexa6do@naver.com>
2023-05-23 19:13:40 +0900

    정렬 책 예제 - 2

---
+++ 이것이코딩테스트다/정렬/성적이 낮은 순서로 학생 출력하기.py
@@ -0,0 +1,13 @@
+n = int(input())
+array = []
+for i in range(n):
+    name, score = input().split()
+    array.append((name, int(score)))
+def sort_definition(data):
+    return data[1]
+
+array.sort(key=sort_definition)
+
+
+for person in array:
+    print(person[0], end=" ")
