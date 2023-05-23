4bfb899b609b7af8c28b5a142b1cbbb330ca366e
hexaspace <hexa6do@naver.com>
2023-05-23 19:14:59 +0900

    정렬 책 예제 - 2 lambda사용 방법

--- 이것이코딩테스트다/정렬/성적이 낮은 순서로 학생 출력하기.py
+++ 이것이코딩테스트다/정렬/성적이 낮은 순서로 학생 출력하기.py
@@ -3,11 +3,11 @@ array = []
 for i in range(n):
     name, score = input().split()
     array.append((name, int(score)))
-def sort_definition(data):
-    return data[1]
+# def sort_definition(data):
+#     return data[1]
 
-array.sort(key=sort_definition)
-
+# array.sort(key=sort_definition)
+array.sort(key = lambda person: person[1])
 
 for person in array:
     print(person[0], end=" ")
