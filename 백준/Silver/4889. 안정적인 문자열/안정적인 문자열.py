24269891494151352fd43b9e8c155128fd486def
hexaspace <hexa6do@naver.com>
2023-05-28 13:22:26 +0900

    [Silver I] Title: 안정적인 문자열, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.py
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.py
@@ -1,10 +1,12 @@
 test_list = []
+# 입력
 while True:
     input_str = input()
     if input_str[0] == '-':
         break
     test_list.append(input_str)
-def check_balance(string):
+    
+def check_balance(string):    #사용안함
     middle = 0
     for s in string:
         if s == '{':
@@ -25,11 +27,11 @@ def turn_balance(string):
         else:
             middle -= 1
         if middle < 0:
-            # 반대방향으로 치환한다고 침
+            # 반대방향 { 으로 치환한다고 침
             middle += 2
             turn_num += 1
      
-    return turn_num + int(middle/2)
+    return turn_num + int(middle/2)   # {가 오버된부분의 개수 middle은, }로 치환하면서 -2씩 빠짐. 그러므로 /2함
 
 # }{
 for t in range(len(test_list)):
